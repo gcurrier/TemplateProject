253ed0f875e1cd331b5336a9f1d0f2c979af80be
glen <jinx3y@gmail.com>
2018-03-25 15:32:16 +0200

    updates to project (build optimization tweaks)

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,22 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: {
     main: './src/index.tsx',
-    vendor: [
-      'lodash','react','react-dom'
-    ]
+    lodash: ['lodash'],
+    react: ['react'],
+    reactDom: ['react-dom']
   },
-  devtool: 'inline-source-map',
-  // devtool: "source-map",
-  //TODO Intellisense does not like the "optimization" option...(Property 'optimization' is not allowed)
-  // optimization: {
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       vendor: {
-  //         name: 'lodash',
-  //         test: 'lodash',
-  //         enforce: true
-  //       }
-  //     }
-  //   },
-  //   runtimeChunk: {
-  //     name: 'manifest'
-  //   }
-  // },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        /* The awesome-typescript-loader prevents the HashedModuleIdsPlugin from actually providing an Id
-           for each splitChunk
-        */
-        // use: 'awesome-typescript-loader',
         // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
         exclude: /node_modules/
       },
@@ -46,12 +28,34 @@ module.exports = {
         */
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      }],
+      }]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
   plugins: [
+    /*Omit the BundleAnalyzerPlugin for production builds - use to view dependency "weight"*/
+    // new BundleAnalyzerPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new UglifyJsPlugin({
+      cache:true,
+      parallel: true,
+      extractComments:true,
+      uglifyOptions:{
+        keep_fnames:true,
+        keep_classnames:true
+      },
+      exclude: [/\.min\.js$/gi] // skip pre-minified libs
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0
+    }),
     // Clean up the dist folder before build (delete it)
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
@@ -64,13 +68,47 @@ module.exports = {
       lang: 'en-US'
     }),
     //Generates a 4-character moduleID in build log output
-    new webpack.HashedModuleIdsPlugin()
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
-    // filename: '[name].js',
-    filename: '[name].[chunkhash].js',
+    filename: '[name].js',
+    // filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  performance: {
+    hints: false
+  },
+  /* Intellisense does not like the "optimization" option...(Property 'optimization' is not allowed) */
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        lodash: {
+          // chunks: 'initial',
+          name: 'lodash',
+          test: 'lodash',
+          enforce: true
+        },
+        react:{
+          name:'react',
+          test:'react',
+          enforce: true
+        },
+        reactDom:{
+          name:'reactDom',
+          test:'reactDom',
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'manifest'
+    }
+  },
+  /* Omit the devtool line for production builds */
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
